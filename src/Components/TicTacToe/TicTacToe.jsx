21e9e35896ab58ecd6d19d2ddaf080fd0e0c2abe
ashishbehera29 <87660082+ashishbehera29@users.noreply.github.com>
2024-09-23 18:36:46 +0530

    Tic-Tac-Toe

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -68,17 +68,17 @@ const TicTacToe = () => {
     <div className="container">
       <h1 className="title" ref={titleRef}>Tic Tac Toe Game In <span> React</span></h1>
       <div className="board">
-        <div className="row1">
+        <div className="row">
           <div className="boxes" onClick={(e) => {toggle(e, 0)}}></div>
           <div className="boxes" onClick={(e) => {toggle(e, 1)}}></div>
           <div className="boxes" onClick={(e) => {toggle(e, 2)}}></div>
         </div>
-        <div className="row2">
+        <div className="row">
           <div className="boxes" onClick={(e) => {toggle(e, 3)}}></div>
           <div className="boxes" onClick={(e) => {toggle(e, 4)}}></div>
           <div className="boxes" onClick={(e) => {toggle(e, 5)}}></div>
         </div>
-        <div className="row3">
+        <div className="row">
           <div className="boxes" onClick={(e) => {toggle(e, 6)}}></div>
           <div className="boxes" onClick={(e) => {toggle(e, 7)}}></div>
           <div className="boxes" onClick={(e) => {toggle(e, 8)}}></div>
